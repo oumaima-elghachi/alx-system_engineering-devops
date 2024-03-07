40afd5837709162136248a9a0851f88faec0bcce
oumaima <elghachiomaima1@gmail.com>
2024-03-07 13:18:39 +0100

    0-1-2-100

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,9 +5,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "underscoDe@alx-holbertonschool"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
